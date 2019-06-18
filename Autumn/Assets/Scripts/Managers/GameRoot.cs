e28960e826698c49a50bfe957d52fcc3cef314c5
JackRFuller <jackrfuller@outlook.com>
2019-06-18 21:20:44 +0200

    DevScene Setup

---
+++ Autumn/Assets/Scripts/Managers/GameRoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRoot : MonoBehaviour
+{
+    private static GameRoot instance;
+    public static GameRoot Instance { get {return instance;}}
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);            
+        }
+    }
+    
+}
